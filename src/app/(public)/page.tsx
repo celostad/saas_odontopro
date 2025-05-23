2e9daec3eec9c686a7374119e826b0c501db1243
mtadim <mtadim@techsocial.com.br>
2025-05-23 18:01:58 -0300

    Seção2 | 9. Criando componente hero landing page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,14 @@
-import { Header } from "./_components/header"
+import { Header } from "./_components/header";
+import { Hero } from './_components/hero'
 
-export default function Home(){
-  return(
-    <div>
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
       <Header />
+
+      <div>
+        <Hero />
+      </div>
     </div>
   )
 }
